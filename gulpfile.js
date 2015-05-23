ba1d623f1c1f4dcd9aa93d5dd405820b5a7312c3
adamelevate <adamelevate@gmail.com>
2015-05-23 12:00:18 -0500

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,32 @@
-//'use strict';
+'use strict';
 
-// --- INIT
-var gulp 			  = require("gulp"),
-	minifyHTML		= require("gulp-minify-html"),
-  plumber       = require('gulp-plumber'),
-  gutil         = require('gulp-util'),
-	//jshint			= require("gulp-jshint"),
-	less 			    = require("gulp-less"),
-	//sourcemaps		= require("gulp-sourcemaps"),
-	autoprefix		= require("gulp-autoprefixer"),
-	rename 			  = require('gulp-rename'),
-	minify 			  = require('gulp-minify-css'), // minifies CSS
-	concat 			  = require('gulp-concat'),
-    uglify 			= require('gulp-uglify'); // minifies JS
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var wrench = require('wrench');
 
-// Paths variables
-var paths = {
-	'dev': {
-        'less': 'public/dev/less/',
-        'js': 'public/dev/js/',
-        'vendor': 'public/dev/vendor/'
-    },
-    'assets': {
-        'css': 'public/assets/css/',
-        'js': 'public/assets/js/',
-        'vendor': 'public/assets/bower_vendor/'
-    }
-}
+var options = {
+  src: 'src',
+  dist: 'dist',
+  tmp: '.tmp',
+  e2e: 'e2e',
+  errorHandler: function(title) {
+    return function(err) {
+      gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+      this.emit('end');
+    };
+  },
+  wiredep: {
+    directory: 'bower_components',
+    exclude: [/jquery/]
+  }
+};
 
-// --- TASKS
-// CSS
-gulp.task('css', function() {  
-  // place code for your default task here
-  return gulp.src(paths.dev.less+'app.less') // get file
-    .pipe(plumber(function(error) {
-            gutil.log(gutil.colors.red(error.message));
-            this.emit('end');
-        }))
-    .pipe(less())
-    .pipe(gulp.dest(paths.assets.css)) // output: app.css
-    .pipe(autoprefix('last 2 version', 'ie 8', 'ie 9'))
-    .pipe(minify({keepSpecialComments:0}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.assets.css)); // output: app.min.css
+wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file)(options);
 });
 
-// JS
-gulp.task('js', function(){  
-  return gulp.src([
-      paths.assets.vendor+'jquery/dist/jquery.js',
-      paths.assets.vendor+'bootstrap/dist/js/bootstrap.js',
-      paths.dev.js+'app.js'
-    ])
-	//.pipe(jshint())
-	//.pipe(jshint.reporter('default'))
-    .pipe(concat('app.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.assets.js));
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
 });
-
-// --- WATCH
-//Rerun the task when a file changes
-gulp.task('watch', function() { 
-	gulp.watch(paths.dev.less+'*', ['css']);
-  
-	gulp.watch(paths.dev.js+'*', ['js']);
-});
-
-// --- DEFAULT
-gulp.task('default', ['css', 'js', 'watch']);
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('scotusChat')
+  .controller('MainCtrl', function ($scope) {
+
+  });

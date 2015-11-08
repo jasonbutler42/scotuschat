a490225ba279bfcdc725fb5ffa74868e7866f8ba
adamelevate <adamelevate@gmail.com>
2015-11-08 17:42:28 -0600

    roughed out speakers page

--- client/app/speakers/speakers.controller.js
+++ client/app/speakers/speakers.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('scotusChatApp')
-  .controller('SpeakersCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('SpeakersCtrl', function ($scope, $http) {
+    
+
+  	$scope.speakers = [];
+    $http.get('/api/speakers/').success(function(speakers) {
+      $scope.speakers = speakers;
+      //socket.syncUpdates('suspects', $scope.suspectList);
+    });
+
+
+});
